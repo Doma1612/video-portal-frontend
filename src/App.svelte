2d8bfa8723fa77c466b8259107c0ab9611066f47
philippkuehling <philippkuehling@gmail.com>
2023-10-06 07:29:14 -0400

    Initialer Commit mit .gitignore, Tailwind Config und Prettier Config

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+  import VideoPlayer from 'svelte-video-player';
+
+
+  
+
+  const source = 'http://techslides.com/demos/sample-videos/small.mp4 ';
+</script >
+
+<main>
+
+<h1>Initialer Commit 610</h1>
+<VideoPlayer width="500" height="500"  {source} loop />
+  
+  
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
+
+  
+
