27691d214a7b48c00aab9047c11cbbc636d08838
philippkuehling <philippkuehling@gmail.com>
2023-10-11 10:33:30 -0400

    Initial Push

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,19 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
-
+  import TailwindCss from './lib/TailwindCSS.svelte';
   import VideoPlayer from 'svelte-video-player';
 
 
   
-
-  const source = 'http://techslides.com/demos/sample-videos/small.mp4 ';
+  const source = 'http://techslides.com/demos/sample-videos/small.webm';
 </script >
 
 <main>
 
-<h1>Initialer Commit 610</h1>
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Initialer Commit 610</h1>
 <VideoPlayer width="500" height="500"  {source} loop />
+
   
   
 
@@ -27,23 +27,6 @@
   </p>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
 
   
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
+  screens: {
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+  },
   plugins: [],
 }
-
