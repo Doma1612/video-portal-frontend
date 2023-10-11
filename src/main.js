581d51e23d1bf04fc2c9e45fc191826840f62303
philippkuehling <philippkuehling@gmail.com>
2023-10-11 13:50:08 -0400

    fix package.json Bug

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
 
-//test
-//test
+// test
+// test
+// test
