cab5d8f5e96891c2f658de1681df03701d0e069a
philippkuehling <philippkuehling@gmail.com>
2023-10-11 14:07:22 -0400

    fix Svelte Eslint Bug

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
@@ -8,25 +8,25 @@
 
   
   const source = 'http://techslides.com/demos/sample-videos/small.webm';
-</script >
+</script > -->
 
 <main>
+  <!-- Test-->
+  <h1
+    class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+  >
+    Initialer Commit 610
+  </h1>
 
-<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Initialer Commit 610</h1>
-<VideoPlayer width="500" height="500"  {source} loop />
-
-  
-  
+  <!-- <VideoPlayer width="500" height="500"  {source} loop /> -->
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-
-  
-
